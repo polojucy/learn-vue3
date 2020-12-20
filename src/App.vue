b859087d8beff9dd17023d21453219848ede34de
polojucy <colorful9767@gmail.com>
2020-12-20 15:40:18 +0800

    columnList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="id">
-    <!-- <img src="./assets/logo.png"/> -->
+  <div class="container">
     <h1>Welcome to Your Vue.js</h1>
-    <h2 v-if="loading">Loading....</h2>
-    <img v-if="loaded" :src="result.message" alt="">
-    <h2>{{count}}</h2>
-    <h2>{{double}}</h2>
-    <h2>mouse position : ({{x}},{{y}})</h2>
-    <button @click="incr">incr count</button>
-    <ul>
-      <li v-for="number in numbers" :key="number">
-        <h2>{{number}}</h2>
-      </li>
-    </ul>
-    <h2 v-if="person.name">person.name is {{person.name}}</h2>
-    <h2>input title</h2>
-    <input type="text" v-model="greating">
+    <column-list :list="list"/>
   </div>
 </template>
 
 <script lang="ts">
 // import { reactive } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const mockList: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'title1',
+    description: 'description1=================',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 2,
+    title: 'title2',
+    description: 'description2=================',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 3,
+    title: 'title3',
+    description: 'description3=================',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 4,
+    title: 'title4',
+    description: 'description4=================',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  }
+]
 export default {
   name: 'App',
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      list: mockList
+    }
+  }
 };
 </script>
 
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+    <div class="col-4 mb-4" v-for="col in list" :key="col.id">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img class="rounded-circle border border-light w-25 my-3" :src="col.avatar" :alt="col.title">
+          <h5 class="card-title">{{col.title}}</h5>
+          <p class="card-text" style="text-align: left">{{col.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true,
+    }
+  },
+  setup(props) {
+    props
+  }
+})
+</script>
