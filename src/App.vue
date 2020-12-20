f2730e9b991358dbca2ada9d86c23ba67feb3640
polojucy <colorful9767@gmail.com>
2020-12-20 10:59:53 +0800

    vue3 reactive & toRefs api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="id">
+    <img src="./assets/logo.png"/>
+    <h1>Welcome to Your Vue.js</h1>
+    <p>{{count}}</p>
+    <p>{{double}}</p>
+    <button @click="incr">click me</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
+import { computed, reactive, toRefs } from 'vue'
+interface DataProps {
+  count: number;
+  incr: () => void;
+  double: number;
+}
+export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    // const count = ref(0)
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // const incr = () => {
+    //   count.value ++
+    // }
+    const data: DataProps = reactive({
+      count: 0,
+      incr: () => data.count ++,
+      double: computed(() => data.count * 2)
+    })
+    return {
+      ...toRefs(data)
+    }
   }
-});
+};
 </script>
 
 <style>
